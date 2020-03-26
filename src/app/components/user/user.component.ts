5e3f3f317fdfe3d467d7f50f26af0484d09d6d61
Brenda-M <brendanjuguna1@gmail.com>
2020-03-26 13:55:55 +0300

    user registration app

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/User'
+
+@Component ({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+
+export class UserComponent implements OnInit {
+  //properties
+  user: User;
+
+  //methods
+  constructor(){
+      
+  }
+  
+  ngOnInit(){
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      email:'john@gmail.com'
+      // age: 30,
+      // address: {
+      //   street: '50 Main st',
+      //   city: 'Boston',
+      //   state: 'MA'
+      // }
+    }  
+  }
+}
+
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { User } from '../../models/User'
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  user: User= {
+    firstName: '',
+    lastName: '',
+    email: ''
+  }
+  users: User [];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+  enableAdd: boolean = false;
+  showUserForm: boolean = false;
+  @ViewChild('userForm')form: any;
+
+  constructor() { }
+
+  ngOnInit() {
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          email: 'john@gmail.com',
+          isActive: true,
+          registered: new Date('01/02/2018 08:30:00'),
+          hide: true
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          email: 'kevin@yahoo.com',
+          isActive: false,
+          registered: new Date('08/07/2002 09:24:00'),
+          hide: true
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          email: 'karen@outlook.com',
+          isActive: true,
+          registered: new Date('11/02/2015 10:30:00'),
+          hide: true
+        }
+  
+      ];
+
+    this.loaded = true;
+  }
+
+  onSubmit({value, valid}: {value: User, valid: boolean}){
+    if (!valid){
+      console.log('Form is not valid');
+    } else {
+      value.isActive = true;
+      value.registered = new Date();
+      this.users.unshift(value);
+      value.hide = true;
+
+      this.form.reset();
+
+    }
+    
+  }
+}
